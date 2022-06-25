f1d2e3bdda38490e12f1ca1df4e243f54cc9a689
NanaYawCodes <yawpokun@gmail.com>
2022-06-25 14:04:19 +0000

    navbar&darkmode

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,60 @@
 import Link from "next/link";
+import ThemeSwitch from "../components/ThemeSwitch";
 
 const Navbar = () => {
     return (
-        <div className="sticky top-0 z-20 py-2 bg-white md:py-6 md:mb-6 dark:bg-black">
-            <div className="container px-4 mx-auto lg:max-w-4xl flex items-center justify-between">
-                <Link href= "/">
-                    <a className= {"font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 uppercase dark:text-white"} >
+        // <div className="flex mb-4 dark:bg-black">
+        //     <div className="w-1/4 bg-gray-500 h-12">
+        //         <Link href= "/">
+        //             <a className= {"font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 uppercase dark:text-white"} >
+        //                 Felix Opoku Osei
+        //             </a>
+        //         </Link>
+        //     </div>
+        //     <div className={"w-1/4 bg-gray-400 h-12"}>
+        //         <Link href="/about">
+        //                 <a className={"block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"}>
+        //                     About
+        //                 </a>
+        //             </Link>
+        //     </div>
+        // </div>
+        <nav className={"flex items-center justify-between flex-wrap bg-black-500 p-8"}>   
+            <div className={"flex items-center flex-shrink-0 text-white mr-6"}>
+                <Link href="/">
+                    <a className={"font-semibold tracking-wider transition-colors text-xl-gray-950 hover:text-sky-500 uppercase dark:text-white"}>
                         Felix Opoku Osei
                     </a>
                 </Link>
             </div>
-        </div>
+            <div className={"w-full block flex-grow lg:flex lg:items-center lg:w-auto"}>
+                <div className={"text-lg lg:flex-grow"}>
+                    <Link href="/">
+                        <a className={"block mt-4 lg:inline-block lg:mt-0 text-gray-600 hover:text-sky-500 mr-4"}>
+                            About Me
+                        </a>
+                    </Link>
+                    <Link href="/">
+                        <a className={"block mt-4 lg:inline-block lg:mt-0 text-gray-600 hover:text-sky-500 mr-4"}>
+                            Projects
+                        </a>
+                    </Link>
+                    <Link href="/">
+                        <a className={"block mt-4 lg:inline-block lg:mt-0 text-gray-600 hover:text-sky-500"}>
+                            Designs
+                        </a>
+                    </Link>
+                </div>
+                <ThemeSwitch />
+                <div>
+                    <Link href="/">
+                        <a className={"inline-block text-lg px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"}>
+                            Contact Me
+                        </a>
+                    </Link>
+                </div>
+            </div>
+        </nav>
     );
 }
  
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from "../components/Layout";
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider attribute="class" enableSystem={false}>
+        <Layout>
+        <Component {...pageProps} />
+        </Layout>
+    </ThemeProvider>
   )
 }
 
