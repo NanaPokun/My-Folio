800de05038bcab0e27b0094190b22b7fe670f807
NanaYawCodes <yawpokun@gmail.com>
2022-06-25 14:15:38 +0000

    footer update

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="max-w-4xl px-4 mx-auto text-gray-800 dark:text-white">
                 <div className="pb-8 mb-2 border-t-2 border-gray-300 dark:border-white-300"></div>
                 <div className="flex flex-col justify-between lg:flex-row items-center">
-                    <p>Built with Next.js, Tailwind and Vercel</p>
+                    <span text-gray-900 font-semibold text-lg mr-2>Yawcodes</span>&copy; {new Date().getFullYear()} All Rights Reserved
                     <div className="flex flex-wrap pt-2 sm:space-x-4 space-x-2 font-medium lg:pt-0">
                         <a
                             href="#"
